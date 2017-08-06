28463d80eab808b412d2960bc9ed61ce1addcd26
mali <sujonpueee@gmail.com>
2017-08-06 19:25:46 -0400

    I did my collage project

---
+++ src/Tuna.java
@@ -0,0 +1,18 @@
+
+public class Tuna {
+	private String girlName;
+	public void setName(String name) {
+		girlName = name;
+	}
+	
+	public String getName() {
+		return girlName;
+		
+	}
+	
+	public void saying() {
+		System.out.printf("your first gf was %s", getName());
+		
+	}
+
+}
